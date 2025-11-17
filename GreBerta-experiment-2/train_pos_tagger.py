b83312e169062d14d1e9af877f57b7073300a6fe
Arjun G. Menon <contact@arjungmenon.com>
2025-11-17 15:32:32 -0500

    Update

--- GreBerta-experiment-2/train_pos_tagger.py
+++ GreBerta-experiment-2/train_pos_tagger.py
@@ -227,7 +227,7 @@ def main():
         per_device_eval_batch_size=args.batch_size,
         num_train_epochs=args.epochs,
         weight_decay=0.01,
-        evaluation_strategy="epoch",
+        eval_strategy="epoch",
         save_strategy="epoch",
         load_best_model_at_end=True,
         metric_for_best_model="accuracy",
